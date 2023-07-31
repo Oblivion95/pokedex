15a07c3c9054216e41d1c6ead125d1ddd8afaa1b
Emmanuel Canto Vazquez <emikogears134@gmail.com>
2023-07-31 00:04:17 -0600

    feat: Add open graph meta tags.

--- src/core/components/layout/main-layout.tsx
+++ src/core/components/layout/main-layout.tsx
@@ -1,13 +1,24 @@
+"use client";
+
 import Head from "next/head";
 import { PropsWithChildren } from "react";
 import Navbar from "../navbar";
 
 type Props = PropsWithChildren<{ title?: string; pokemon?: string }>;
 
-const MainLayout = ({ children, title = "", pokemon }: Props) => (
+const MainLayout = ({ children, title = "", pokemon }: Props) => {
+
+  const location = typeof window === "undefined" ? "" : window.location.origin;
+
+  console.log(location)
+
+  return (
   <>
     <Head>
-      <title>{title || 'Pokemon App'}{title}</title>
+      <title>
+        {title || "Pokemon App"}
+        {title}
+      </title>
       <meta
         name="description"
         content={
@@ -17,10 +28,22 @@ const MainLayout = ({ children, title = "", pokemon }: Props) => (
         }
       />
       <meta name="keywords" content="pokemon, pokedex" />
+      <meta
+        property="og:title"
+        content={`Informació sobre el pokemon ${pokemon}`}
+      />
+      <meta
+        property="og:description"
+        content="Get from SEO newbie to SEO pro in 8 simple steps."
+      />
+      <meta
+        property="og:image"
+        content={`${location}/img/banner.png`}
+      />
     </Head>
     <Navbar />
     <main>{children}</main>
   </>
-);
+)};
 
 export default MainLayout;
